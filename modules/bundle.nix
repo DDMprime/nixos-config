fac9617bd3b302a80cf93e6f795d7f8a30281f76
DDM_prime <DDM_prime@proton.com>
2025-05-05 06:16:18 +0500

    25.05.05-1

--- modules/bundle.nix
+++ modules/bundle.nix
@@ -1,12 +1,12 @@
 {
   imports = [
     ./common/packages.nix
+    ./common/steam.nix
     ./enivroinments/hyprland.nix
     ./enivroinments/zsh.nix
     ./hardware/nvidia-gpu.nix
     ./services/autorun_input_remapper.nix
     ./services/pipewire.nix
-    ./services/steam.nix
     ./services/zerotier.nix
     ./services/tailscale.nix
     ./services/zapret.nix
