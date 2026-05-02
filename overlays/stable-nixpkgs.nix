e07edd65be203201cc1de3fd426d6c551995b2ee
DDM_prime <DDM_prime@proton.com>
2026-05-02 18:40:34 +0500

    02.04.2026-1

--- overlays/stable-nixpkgs.nix
+++ overlays/stable-nixpkgs.nix
@@ -2,4 +2,5 @@
 
 final: prev: {
     stable = inputs.nixpkgs-stable.legacyPackages.${prev.system}; #добавляем оверлей для стабильной ветки. Вызывается с помощью pkgs.stable
+  hyprsession = inputs.hyprsession.packages.${prev.system}.hyprsession;
 }
